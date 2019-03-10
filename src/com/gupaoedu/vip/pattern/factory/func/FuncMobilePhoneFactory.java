d1a4e0637b1e0b1db44b13d4a6f01bc8994ce6dd
ZZ <zz99132242@qq.com>
2019-03-10 14:46:14 +0800

    2019-03-10 14:42:30

---
+++ src/com/gupaoedu/vip/pattern/factory/func/FuncMobilePhoneFactory.java
@@ -0,0 +1,12 @@
+package com.yc.factory.func;
+
+import com.yc.factory.IMobilePhone;
+
+/**
+ * @author Yanchen
+ * date 2019/3/10 13:33
+ */
+public interface FuncMobilePhoneFactory {
+
+    public IMobilePhone createMobilePhone();
+}
